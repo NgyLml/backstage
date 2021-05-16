0e2cfee9a42ae8bd64e9f12ba451f1918d68ee6a
lml <154616940@qq.com>
2021-05-16 21:36:33 +0800

    'container不行'

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import ElementUI from "element-ui";
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-
+    我是home的内容
   </div>
 </template>
 
